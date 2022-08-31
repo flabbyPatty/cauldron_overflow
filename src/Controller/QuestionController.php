6e0b5360e84862e5c85fd8a505c7e9c0b8b71ee8
Patrick Santos <pgsantos.92@gmail.com>
2022-08-31 07:19:27 +0000

    Autowire dependencies into the markdown helper service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,11 +3,9 @@
 namespace App\Controller;
 
 use App\Service\MarkdownHelper;
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -30,7 +28,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $answers = [
             'Make sure your cat is sitting `purrfectly` still 🤣',
@@ -40,8 +38,6 @@ class QuestionController extends AbstractController
         $questionText = "I've been turned into a cat, any *thoughts* on how to turn back? While I'm **adorable**, I don't really care for cat food.";
 
         $parsedQuestionText = $markdownHelper->parse($questionText);
-
-        dump($cache);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
